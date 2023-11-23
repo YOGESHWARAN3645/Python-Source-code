32c1f4a7b37f78cde7a2c192060b541eb43d13b2
YOGESHWARAN D <124228144+YOGESHWARAN3645@users.noreply.github.com>
2023-11-23 12:37:14 +0530

    Update class and objects exercises.py

--- oops/class and objects exercises.py
+++ oops/class and objects exercises.py
@@ -25,13 +25,53 @@ s2.display()
 #create a object called apple "pass the color variable as a parameter through object"
 
 class Fruits:
-  def __init__(self):
-    self.color=""
+  def __init__(self,col):
+    self.color=col
   def display(self):
     print(self.color)
 
-apple=Fruits()
-apple.color= "red"
+apple=Fruits("red")
+apple.display()
+
+#create a class called Teacher
+#create a variable name and register number using constructor.
+#create a function called dsiplay which should display the name and register number of teacher.
+class Teacher():
+    def __init__(self,name,regNo):
+        self.name=name
+        self.regNo=regNo
+    def display(self):
+        print("Name : ",self.name)
+        print("Regsiter number : ",self.regNo)
+
+t1 = Teacher("shalini",56)
+t2= Teacher("rohini",57)
+t1.display()
+t2.display()
+
+#create a class called calculator
+#create a 2 variable a and b
+#create a function called add, sub, mul, div, should take two variables as a parameter.
+#pass thr a and b valur through object()
+class Calculator():
+    def add(self,a,b):
+        print(a+b)
+    def sub(selfa,b):
+        print(a+b)
+    def mul(self,a,b):
+        print(a+b)
+    def div(self,a,b):
+        print(a+b)
+num1= Calculator()
+num2= Calculator()
+a=int(input("A:"))
+b=int(input("B:"))
+num1.add(a,b)
+      
+    
+
+
+
 
 
 
