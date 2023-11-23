5903f8359d5c861ba6800dc828ec5accf4d104cf
YOGESHWARAN D <124228144+YOGESHWARAN3645@users.noreply.github.com>
2023-11-23 09:53:44 +0530

    on going

---
+++ oops/class and objects exercises.py
@@ -0,0 +1,37 @@
+#create a class called Student.
+#create a variable name and register number.
+#create a function called diplay which should display the name and register num of the student.
+
+
+class Student():
+    def __inti__(self):
+        self.name-""
+        self.regNo=0
+    def display(self):
+        print("Name: ",self.name)
+        print("Reg No: ",self.regNo)
+s1= Student()
+s1.name="yogesh"
+s1.regNo=36
+s2= Student()
+s2.name="Rohit sharma"
+s2.regNo=45
+
+s1.display()
+s2.display()
+
+#create a class called Fruits
+#create a variable called color using __init__ method.
+#create a object called apple "pass the color variable as a parameter through object"
+
+class Fruits:
+  def __init__(self):
+    self.color=""
+  def display(self):
+    print(self.color)
+
+apple=Fruits()
+apple.color= "red"
+
+
+
